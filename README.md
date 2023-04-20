55fc2f7d792cb4b08b780db8a0af88ae685dbca5
JoTYF <joan_tta@live.com.sg>
2023-04-20 15:45:06 +0800

    notification page first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Frontend Mentor - Notifications page solution
+
+This is a solution to the [Notifications page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/notifications-page-DqK5QAmKbC). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Distinguish between "unread" and "read" notifications
+- Select "Mark all as read" to toggle the visual state of the unread notifications and set the number of unread messages to zero
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- Able to append/add as many notifications in the json file and the interface will still work i.e. show and update the number of unread notifications
+
+### Screenshot
+
+- Original: ![./assets/images/original.png](./assets/images/original.png)
+
+- After adding new notifications: ![./assets/images/after.png](./assets/images/after.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [Styled Components](https://styled-components.com/) - For styles
+
+### What I learned
+
+On the first glance, this is a simple challenge with little javascript requirements and bulk of the requirements can be met using HTML & CSS alone.
+
+However, I wanted to give myself more challenge by making this notification page slightly 'smarter'. The whole idea is not to rely on HTML for static information, but to instead transfer the data on the HTML file and put them into a json file where I can iterate using Javascript and even add more information if I want to. This is also to brush up my Javascript skills.
+
+I started by noticing that there is a pattern in the notification messages and use it to sort the data into key-value pairs in the json file. This allows me to set up a for loop and conditional statements to check for certain conditions and update the interface accordingly.
+
+Next would be to draft out the expected html structure you want to see when each of the conditions happen. This guided me how to structure my lines in javascript.
+
+The for loop is a reverse one because the data in the json file is the reverse of what users see on the interface. If there are new notifications coming in, usually it appears at the top of the interface. However, in the json file, it is appended at the end of the list of the data. Thus, when iterating through the number of items in the data, we have to do the reverse way. It might seemed like a counter-intuitve step as information like time is still static in this case. Nevertheless, for this purpose, it is to allow users to add information to test the page. Of course, the 'smartes' way to make this page work will have to involve integrating a clock and doing backend stuff.
+
+The trickiest part for me in this whole approach comes in the sequencing. The sequence of which the elements or divs gets added to the main div step by step is very important. In addition, it is crucial to know which item(s) need to happen within the for loop, which ones have to happen outside the for loop. The 'redDot' is a key example.
+
+Overall, I am glad I gave myself this challenge and made it through. I hope this will set a foundation for me when I am more familiarize with backend technologies like node.js and mongoDB, as well as, APIs.
+
+## Author
+
+- Frontend Mentor - [@Jo-cloud85](https://www.frontendmentor.io/profile/Jo-cloud85)
+
+## Acknowledgments
+
+Background photo by [Jonathan Bean on Unsplash](https://unsplash.com/photos/sbZU1j31ggE)
---
+++ script.js
@@ -0,0 +1,140 @@
+const nBoxes = document.querySelector('.notifications');
+const marBtn = document.querySelector('#mar-btn');
+const header = document.querySelector('header');
+const nNum = document.querySelector('.n-num'); 
+
+async function loadNotification() {
+    nBoxes.innerHTML = "";
+    let response = await fetch('notifications.json');
+    let notifications = await response.json();
+
+    let numOfUnreadN = 0 //to count all the unread notifications later
+    
+    const redDots = []; //create an array to store all redDot elements
+
+    for (let i=notifications.length-1; i>=0; i--) {
+
+        //extract the respective info from json file
+        const nameA = notifications[i].name;
+        const lcName = nameA.toLowerCase();
+        const activityRA = notifications[i].activity.reaction;
+        const activityDA = notifications[i].activity.description;
+        const status = notifications[i].status;
+        const timeAgo = notifications[i].time;
+        
+        //create a div called nBox for each notification
+        const nBox = document.createElement('div'); //This is for each nBox
+        
+        //create elements within each nBox div
+        const profileImg = document.createElement('img'); //this is for their profile pic in each nBox
+        const info = document.createElement('div'); //this is for all the info in each nBox
+        
+        //add class to nBox div
+        nBox.classList.add('nBox'); //nBox css will create the box first
+
+        //img (sub-div of nBox-div)
+        profileImg.src = `./assets/images/avatar-${lcName.replace(/\s+/g, '-')}.webp`;
+        profileImg.alt = `${lcName.replace(/\s+/g, '-')}-avatar`;
+
+        //add class to info (sub-div of nBox-div)
+        info.classList.add('info');
+
+        //create elements within info div
+        const activity = document.createElement('div');
+        const time = document.createElement('p');
+
+        //add classes to activity, time div (sub-divs of info)
+        activity.classList.add('activity');
+        time.classList.add('time');
+        
+        //create element(s) & add classes within activity div
+        const name = document.createElement('span');
+        const activityR = document.createElement('span');
+        const activityD = document.createElement('span');
+        
+        //add classes & set text to name, activityR (sub-divs of activity div), classes for activityD to add later due to special conditions
+        name.classList.add('name');
+        name.textContent = nameA;
+
+        activityR.classList.add('activity-r');
+        activityR.textContent = activityRA;
+        
+        //set text to time div
+        time.textContent = timeAgo;
+
+        //joining parts of divs within activity div & using if else to check condition
+        let lastWord = activityRA.split(" ").pop();
+        
+        activityD.textContent = activityDA;
+        
+        //append first because the sequence of elements or divs appended matters
+        activity.appendChild(name);
+        activity.appendChild(activityR);
+        activity.appendChild(activityD);
+
+        info.appendChild(activity);
+        info.appendChild(time);
+
+        if (lastWord === 'post') {
+            activityD.classList.add('activity-d-gray');
+        } else if (lastWord === 'group') {
+            activityD.classList.add('activity-d-blue');
+        } else if (lastWord === 'message') {
+            //create an extra div and add class (extra sub-div of info)
+            const privateMsg = document.createElement('div');
+            privateMsg.classList.add('privateMsg');
+            //create element & add class within privateMsg div
+            const privateM = document.createElement('p');
+            privateM.classList.add('privateMsg-text');
+            activityD.textContent = '';
+            privateM.textContent = activityDA;
+            privateMsg.appendChild(privateM);
+            info.appendChild(privateMsg);
+        }
+
+        if (status === "read") {
+            nBox.classList.add('nBox-transparent-bg') //light blue box is just the default bg color if unread
+        } else {
+            const redDot = document.createElement('span');
+            redDot.classList.add('red-dot');
+            activity.appendChild(redDot);
+            numOfUnreadN++; //to count all the unread notifications that is to be shown on the page
+            redDots.push(redDot); //push new elements into the redDots array
+        }
+
+        //append first because the sequence of elements or divs appended matters
+        nBox.appendChild(profileImg);
+        nBox.appendChild(info);
+
+        if (lastWord === 'picture') {
+            activityD.textContent = '';
+            const pic = document.createElement('img');
+            pic.classList.add('pic-img');
+            pic.src = activityDA;
+            pic.alt = "pic-img";
+            nBox.appendChild(pic);
+            nBox.classList.remove('nBox');
+            nBox.classList.add('nBox-v2'); //for reformating to add 1 more column for the pic
+        }
+
+        nBoxes.appendChild(nBox);
+    }
+
+    nNum.textContent = numOfUnreadN //this line has to be out of the for loop
+
+    //mark-all-as-read button which serves as a toggle
+    marBtn.addEventListener('click',() => {
+        
+        nNum.classList.toggle('n-num');
+        nNum.classList.toggle('n-num-toggle');
+
+        nBoxes.querySelectorAll('.nBox').forEach((nBox, i) => {
+            nBox.classList.toggle('nBox-transparent-bg-toggle');
+            if (redDots[i]) {
+                redDots[i].classList.toggle('red-dot');
+            }
+        });
+    });
+}
+
+loadNotification();
